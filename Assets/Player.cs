1a917e280e13ef44acadc2692ff68cbfa2a324e2
Suzu_Yoshi <acver.rainbox@gmail.com>
2020-06-25 23:18:14 +0900

    別クラスの値を取得

---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//プレイヤーのクラス
+public class Player : MonoBehaviour
+{
+    //メンバ***************************
+    private int HP; //HPは誰かに操作されると困る・・・困りませんか？？
+
+    //コンストラクタ
+    public Player()
+    {
+        HP = 10;    //HPを10に設定
+    }
+
+    //デストラクタ
+    ~Player()
+    {
+        //処理なし
+    }
+
+    //メソッド***************************
+
+    //HPをゲットする
+    public int GetHP()
+    {
+        return this.HP; //HPを返す
+    }
+
+    //HPを減らすする
+    public void MinusHP(int DownValue)
+    {
+        this.HP -= DownValue; //HPを減算する
+    }
+}
---
+++ Assets/ViewHP.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;  //追加
+
+public class ViewHP : MonoBehaviour
+{
+    //Textオブジェクト
+    //Unityから、Inspector内で、Textをドラッグアンドドロップで追加すること
+    public GameObject score_object = null;
+
+    //ゲームオブジェクト
+    //Unityから、Inspector内で、playerをドラッグアンドドロップで追加すること
+    public GameObject game_object;
+
+    //実際に表示するテキスト
+    private Text score_text;
+
+    //実際に取得するPlayerクラス
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // オブジェクトからTextコンポーネントを取得
+        score_text = score_object.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //マウスの左ボタンをクリックしたら・・
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Playerクラス取得
+            player = game_object.GetComponent<Player>();
+
+            if (player != null) //クラスが取得できたら
+            {
+                //HPを減らす
+                player.MinusHP(1);
+            }
+        }
+
+        //HPを表示する
+        score_text.text = player.GetHP().ToString();
+    }
+}
